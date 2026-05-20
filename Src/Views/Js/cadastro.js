3390ead928c11b6ea626381e509bc174837d2c85
Moraes-code <bruno_l_moraes@estudante.sesisenai.org.br>
2026-05-20 11:24:49 -0300

    ADD - página home adicionada e sendo constantemente atualizada pelos integrantes do grupo

--- Src/Views/Js/cadastro.js
+++ Src/Views/Js/cadastro.js
@@ -1,21 +1,34 @@
-document.getElementById("formCadastro").addEventListener("submit", function(event) {
-  event.preventDefault(); 
+const form = document.getElementById("formLogin");
 
-  const nome = document.getElementById("nome").value;
-  const email = document.getElementById("email").value;
-  const senha = document.getElementById("senha").value;
-  const confirmarSenha = document.getElementById("confirmarSenha").value;
+form.addEventListener("submit", function(event){
 
-  
-  if (senha !== confirmarSenha) {
-    alert("As senhas não coincidem!");
-    return;
-  }
+    event.preventDefault();
 
-  
-  localStorage.setItem("email", email);
-  localStorage.setItem("senha", senha);
+    const nome = document.getElementById("nome").value;
+    const email = document.getElementById("email").value;
+    const senha = document.getElementById("senha").value;
 
-  
-  window.location.href = "login.html";
+    if(nome.length < 6){
+
+        alert("O nome precisa ter no mínimo 6 caracteres!");
+        return;
+
+    }
+
+    if(!email.includes("@")){
+
+        alert("Digite um e-mail válido!");
+        return;
+
+    }
+
+    if(senha.length < 6){
+
+        alert("A senha precisa ter no mínimo 6 caracteres!");
+        return;
+
+    }
+
+    window.location.href = "login.html";
+
 });
