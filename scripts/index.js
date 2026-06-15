4f9cc416e10cf1d2bf98165197c965c05c324227
andré <andre_f_alflen@estudante.sesisenai.org.br>
2026-06-15 08:32:43 -0300

    ADD - Java Sccript da pagina de login iniciada.

--- scripts/index.js
+++ scripts/index.js
@@ -2,3 +2,11 @@ function entrar() {
     window.location.href = "public/cadastro.html";
 }
 
+
+document.addEventListener("DOMContentLoaded", () => {
+
+     
+    const formulario = document.getElementById("Login");
+    const emailInput = document.getElementById("email");
+    const senhaInput = document.getElementById("senha");
+    const nomeInput = document.getElementById("nome");
